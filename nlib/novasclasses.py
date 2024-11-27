3d2e4269014b4b7c4feb0193b57b0aa376cdad89
S00006884 <g.hickman2022@gmail.com>
2024-11-27 12:23:38 +0000

    adding tictactoe class and adding logic

--- nlib/novasclasses.py
+++ nlib/novasclasses.py
@@ -7,19 +7,22 @@ class Book():
         self.status = status
 
 class ticSpace():
-    def __init__(self, x: int, y: int, isAnX: bool, isAnO: bool):
+    def __init__(self, x: int, y: int, isAnX: bool, isAnO: bool, isAvailable: bool = True):
         self.x = x
         self.y = y
         self.isAnX = isAnX
         self.isAnO = isAnO
+        self.isAvailable = isAvailable
     def changeToX(self, x, y):
         self.x = x
         self.y = y
         self.isAnX = True
         self.isAnO = False
+        self.isAvailable = False
     def changeToO(self, x, y):
         self.x = x
         self.y = y
         self.isAnO = True
         self.isAnX = False
+        self.isAvailable = False
 
--- projects/tictactoes/ticetytacity.py
+++ projects/tictactoes/ticetytacity.py
@@ -1,14 +1,20 @@
 from nlib import novasroutines as nr, novasclasses as nc
 
 
-class ticBoard():
+class TicBoard():
     def __init__(self):
+        pass
+    def boardCreate(self):
         board = [[nc.ticSpace(0, 0, False, False), nc.ticSpace(1, 0, False, False), nc.ticSpace(2, 0, False, False)],
                  [nc.ticSpace(0, 1, False, False), nc.ticSpace(1, 1, False, False), nc.ticSpace(2, 1, False, False)],
                  [nc.ticSpace(0, 2, False, False), nc.ticSpace(1, 2, False, False), nc.ticSpace(2, 2, False, False)]]
         return board
 
+ticBoard = TicBoard().boardCreate()
+for i in range(len(ticBoard)):
+    for j in range(len(ticBoard[i])):
+        space = ticBoard[i][j]
+        if not(space.isAnX) and not(space.isAnO):
+            ticBoard[i][j].isAvailable = True
 
 
-ticBoard = ticBoard()
-print(ticBoard)
