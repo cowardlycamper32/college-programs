8040a3eda74617d6904e51930101581ce4d77885
GHickman <g.hickman2022@gmail.com>
2024-12-14 11:01:30 +0000

    temp

--- projects/tictactoes/ticetytacity.py
+++ projects/tictactoes/ticetytacity.py
@@ -7,6 +7,7 @@ from nlib import novasroutines as nr, novasclasses as nc
 class TicBoard():
     def __init__(self):
         pass
+
     def boardCreate(self):
         board = [[nc.ticSpace(0, 0, False, False), nc.ticSpace(1, 0, False, False), nc.ticSpace(2, 0, False, False)],
                  [nc.ticSpace(0, 1, False, False), nc.ticSpace(1, 1, False, False), nc.ticSpace(2, 1, False, False)],
@@ -20,45 +21,48 @@ def checkIfINITCorrect(board):
         for j in range(len(board[i])):
             space = board[i][j]
             if not(space.isAnX) and not(space.isAnO):
-                board[i][j].isAvailable = True
+                space.isAvailable = True
                 count += 1
     if count == 9:
         return True
     else:
         return False
 
+
 def inputthingie(input):
     splitthing = nr.splitter(input, ",")
-    for i in range(len(splitthing)):
-        if nr.intcheck(splitthing[i]):
-            splitthing[i] = int(splitthing[i])
-        else:
-            return False
-    return splitthing
+    while not all(nr.intcheck(item) for item in splitthing):
+        print("Enter the coordinates please")
+        inputthingie(input())
+    return [int(x) for x in splitthing]
+
 
 def inputLoop(board, display):
-    displayBoardCLI(board, display)
     count = 0
     finished = False
     while not(finished):
-        splat = inputthingie(input("enter the coordinate in the format \'X,Y\'."))
-        if splat == False:
-            print("enter the coordinates please")
+        splat = inputthingie(input("Enter the coordinate in the format 'X,Y' (or 'quit' to exit): "))
+        if splat == ['quit']:
+            print('Game exited')
+            break;
         elif len(splat) != 2:
-            print("enter the coordinates as instructed please")
+            print("Enter the coordinates as instructed please")
         else:
             if whosTurn(count):
                 if board[splat[0]][splat[1]].isAvailable:
                     board[splat[0]][splat[1]].isAvailable = False
                     board[splat[0]][splat[1]].isX = True
                     count += 1
-                    displayBoardCLI(board, display)
             elif not(whosTurn(count)):
                 if board[splat[0]][splat[1]].isAvailable:
                     board[splat[0]][splat[1]].isAvailable = False
                     board[splat[0]][splat[1]].isO = True
                     count += 1
-                    display = displayBoardCLI(board, display)
+            else:
+                finished = True
+
+    displayBoardCLI(board, display)
+
 
 def whosTurn(count):
     if count % 2 == 0 or count == 0:
@@ -67,24 +71,28 @@ def whosTurn(count):
         return False # return False for O
 
 
-
 def displayBoardCLI(board, display):
 
-    count = 0
     for i in range(len(board)):
         for j in range(len(board[i])):
             space = board[i][j]
-            display[i][j] = space.currentToken
-    #os.system('cls')
+            if display[i][j] == " ":
+                display[i][j] = ""
+            elif display[i][j] == "X":
+                display[i][j] = "O"
     nr.print2Dnicely(display)
+    print()
     return display
+
+
 initdisplay = [[" ", " ", " "],
                [" ", " ", " "],
                [" ", " ", " "]]
 ticBoard = TicBoard().boardCreate()
-print(checkIfINITCorrect(ticBoard))
-print(inputLoop(ticBoard, initdisplay))
+if checkIfINITCorrect(ticBoard):
+    inputLoop(ticBoard, initdisplay)
+
+else:
+    print("The initial board is not correct")
+
 print("This is a test")
-
-
-
