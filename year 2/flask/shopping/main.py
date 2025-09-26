4db0cdaad641a1c7c6ebe21206f3b32bd556d56b
nova <nova@cutey.com>
2025-09-26 13:00:55 +0100

    work ig

--- year 2/flask/shopping/main.py
+++ year 2/flask/shopping/main.py
@@ -5,22 +5,30 @@ import sqlite3 as db
 import requests
 from flask_wtf import FlaskForm, CSRFProtect, RecaptchaField
 from wtforms import StringField, SubmitField, PasswordField, FileField
+from wtforms.fields.simple import EmailField
 from wtforms.validators import DataRequired, Length, EqualTo
 import flask_bootstrap
+import flask_argon2 as ar2
 app = Flask(__name__)
-
+secretFile = open('secret.scrt', 'r')
+app.secret_key = secretFile.read()
+secretFile.close()
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 class Product:
-    def __init__(self, name, price, description, id):
+    def __init__(self, name, price, description, id, image):
         self.name = name
         temp = price / 100
         self.price = "$" + str(temp)
         self.description = description
         self.id = "/product/" + str(id)
+        if image == "none" or image is None:
+            self.imageLocation = "images/placeholder.png"
+        else:
+            self.imageLocation = "images" + image
 
 @app.route("/search")
 def search():
@@ -29,12 +37,14 @@ def search():
     with db.connect("database.db") as database:
         cur = database.cursor()
         if query is None:
-            results = cur.execute("SELECT * FROM Products")
+            results = cur.execute("SELECT * FROM Products").fetchall()
         else:
             results = cur.execute("SELECT * FROM Products WHERE instr(UPPER(Title), UPPER(?)) > 0 OR instr(UPPER(Description), UPPER(?)) > 0", (escape(query),escape(query))).fetchall()
         print(products)
         for product in results:
-            products.append(Product(product[1], product[3], product[2], product[0]))
+            imageID = product[4]
+            imageURL = cur.execute("SELECT * FROM Images WHERE ImageID = ?", (imageID,)).fetchone()[1]
+            products.append(Product(product[1], product[3], product[2], product[0], imageURL))
 
 
 
@@ -45,8 +55,9 @@ def product(id):
     with db.connect("database.db") as database:
         cur = database.cursor()
         product = cur.execute("SELECT * FROM Products WHERE ID = ?", (id,)).fetchone()
+        productImage = cur.execute("SELECT location FROM Images WHERE ImageID = ?", (str(product[4]))).fetchone()
     try:
-        prod = Product(product[1], product[3], product[2], product[0])
+        prod = Product(product[1], product[3], product[2], product[0], productImage[0])
         print(product)
         return render_template("product.html", product=prod)
     except TypeError:
@@ -71,6 +82,48 @@ def about():
 def contact():
     return "SERIOUSLY FUCK OFF ASSHOLE"
 
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email Address")
+    password = PasswordField("Password", validators=[DataRequired()])
+    repeatPassword = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+
+    submit = SubmitField("submit")
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    usernameError = "none"
+    repeatPasswordError = "none"
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        repeatPassword = form.repeatPassword.data
+
+        with db.connect("database.db") as database:
+            cur = database.cursor()
+            userCheck = cur.execute("SELECT username FROM users WHERE username = ?", (username)).fetchone()
+
+            if username == userCheck[0]:
+                doNotRun = True
+                usernameError = "That username is in use."
+            else:
+                doNotRun = False
+
+            if password != repeatPassword:
+                doNotRun = True
+                repeatPasswordError = "Passwords must match"
+            else:
+                doNotRun = False
+            if not doNotRun:
+                pass
+
+    return render_template("register.html", form=form, usernameError=usernameError, repeatPasswordError=repeatPasswordError)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
@@ -82,7 +135,7 @@ class SearchForm(FlaskForm):
 class LoginForm(FlaskForm):
     pass
 
-class RegisterForm(FlaskForm):
-    pass
 
+
+    submit = SubmitField("submit")
 
